54ea29de69bb5c7c5c37ae9e28c060682ed98475
Omkar Kathile <omkarkathile007@gmail.com>
2025-02-03 23:08:44 +0530

    build landing page and setup prisma

---
+++ frontend/app/(main)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout({children}) {
+  return (
+    <div className='container mx-auto mt-24 mb-20'>{children}</div>
+  )
+}
+
+export default layout
---
+++ frontend/app/layout.js
@@ -0,0 +1,42 @@
+import {Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { headers } from "next/headers";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const inter=Inter({subsets:['latin']});
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={{
+       baseTheme:dark,
+    }}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* {headers} */}
+            <Header/>
+            <main className="min-h-screen">{children}</main>
+            <footer className="text-white bg-muted/50 py-12">
+                <div className="container mx-auto px-4 text-center text-gray-200">
+                  <p>Made with love by Omkar</p>
+                </div>
+            </footer>
+      </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
